ea09b83c5f28b3afabf290a41d05c00aebe04845
Zachary Moritz <zackmoritz94@gmail.com>
2025-08-09 22:34:11 -0500

    Launch

--- Reign_PWA/sw.js
+++ Reign_PWA/sw.js
@@ -1,4 +1,4 @@
 // at the top of sw.js
 const CACHE_NAME = 'reign-cache-v1-2-18'; // bump this
 // sw.js v1.2.16
-const CACHE_NAME = 'reign-cache-v1-2-16';
+const CACHE_NAME = 'reign-cache-v1-2-18';
